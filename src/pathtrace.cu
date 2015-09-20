8456e04ab10996bb22e21624c08bd17dc60084b3
Kai Ninomiya <kainino1@gmail.com>
2015-09-20 19:48:27 -0400

    Oops I forgot how to #endif

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -32,7 +32,7 @@ void checkCUDAErrorFn(const char *msg, const char *file, int line) {
     }
     fprintf(stderr, ": %s: %s\n", msg, cudaGetErrorString(err));
     exit(EXIT_FAILURE);
-#endif ERRORCHECK
+#endif
 }
 
 __host__ __device__
