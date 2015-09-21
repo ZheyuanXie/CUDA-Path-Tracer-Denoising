cf1219b8e08b4811ce62c10f621d1f14b6986b42
Kai Ninomiya <kainino1@gmail.com>
2015-09-21 15:58:03 -0400

    Pause after error on Windows so you can read the error message

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -31,6 +31,9 @@ void checkCUDAErrorFn(const char *msg, const char *file, int line) {
         fprintf(stderr, " (%s:%d)", file, line);
     }
     fprintf(stderr, ": %s: %s\n", msg, cudaGetErrorString(err));
+#  ifdef _WIN32
+    getchar();
+#  endif
     exit(EXIT_FAILURE);
 #endif
 }
